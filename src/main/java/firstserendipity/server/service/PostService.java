0f8922ee3f071c6135ae74f488c93caa583023b9
hyun <h.zoo3963>
2023-07-15 20:03:33 +0900

    feat : Post service createPost

--- src/main/java/firstserendipity/server/service/PostService.java
+++ src/main/java/firstserendipity/server/service/PostService.java
@@ -1,8 +1,29 @@
 package firstserendipity.server.service;
 
+import firstserendipity.server.domain.dto.request.RequestPostDto;
+import firstserendipity.server.domain.dto.response.ResponsePostDto;
+import firstserendipity.server.domain.entity.Post;
+import firstserendipity.server.repository.PostRespository;
+import firstserendipity.server.util.JwtUtil;
+import firstserendipity.server.util.mapper.PostMapper;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class PostService {
 
+    private final PostRespository postRespository;
+
+    public ResponsePostDto createPost(RequestPostDto requestPostDto) {
+        // RequestDto -> Entity
+        Post post = PostMapper.INSTANCE.toEntity(requestPostDto);
+        // DB 저장
+        Post savePost = postRespository.save(post);
+        // Entity -> ResponseDto
+        ResponsePostDto responsePostDto =  PostMapper.INSTANCE.toResponseDto(post);
+
+        return responsePostDto;
+    }
 }

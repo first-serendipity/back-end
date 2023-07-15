588c06018e926374c5f95f38f87083df9d693a45
hyun <h.zoo3963>
2023-07-16 00:11:02 +0900

    feat : PostRespository.java body : 삭제 후 재생성

--- src/main/java/firstserendipity/server/repository/PostRespository.java
+++
@@ -1,8 +0,0 @@
-package firstserendipity.server.repository;
-
-import firstserendipity.server.domain.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRespository extends JpaRepository<Post,Long> {
-
-}

04c922e74bd0ef2c136dd21063a587f95f8aaa6c
princeton <ssk0080@gmail.com>
2023-07-15 16:56:00 +0900

    Feat: create signup dto to entity mapping

--- src/main/java/firstserendipity/server/util/mapper/MemberMapper.java
+++ src/main/java/firstserendipity/server/util/mapper/MemberMapper.java
@@ -1,4 +1,17 @@
 package firstserendipity.server.util.mapper;
 
+import firstserendipity.server.domain.dto.request.RequestMemberSignupDto;
+import firstserendipity.server.domain.entity.Member;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface MemberMapper {
+
+    MemberMapper INSTANCE = Mappers.getMapper(MemberMapper.class);
+
+    @Mapping(target = "role", constant = "MEMBER")
+    Member requestMemberSignupDtoToEntity(RequestMemberSignupDto requestDto);
+
 }

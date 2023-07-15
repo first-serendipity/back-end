27bb5b4aa4afe827b952b0696cde690856aacf25
hyun <h.zoo3963>
2023-07-16 01:53:56 +0900

    feat : PostController 전체조회

--- src/main/java/firstserendipity/server/controller/PostController.java
+++ src/main/java/firstserendipity/server/controller/PostController.java
@@ -8,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -17,11 +20,14 @@ public class PostController {
 
     // 나영님의 게시글 작성
     @PostMapping("/posts")
-//    @PreAuthorize("hasRole('ROLE_NAYOUNG')")
-    public ResponseEntity<ResponsePostDto> createPost(@RequestBody RequestPostDto requestPostDto, HttpServletRequest req) {
-        return null;
+    public ResponsePostDto createPost(@RequestBody RequestPostDto requestPostDto, HttpServletRequest req) {
+        return postService.createPost(requestPostDto,req);
     }
     // 전체 게시글 조회
+    @GetMapping("/posts")
+    public List<ResponsePostDto> getPosts(){
+        return postService.getPosts();
+    }
     // 인기글 게시글 조회
     // 추천 게시글 조회
 

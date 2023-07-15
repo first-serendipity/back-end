376c68879bc649b9d7534f19d24f9c5b93a05070
princeton <ssk0080@gmail.com>
2023-07-15 09:50:02 +0900

    init

---
+++ src/test/java/firstserendipity/server/ApplicationTest.java
@@ -0,0 +1,13 @@
+package firstserendipity.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

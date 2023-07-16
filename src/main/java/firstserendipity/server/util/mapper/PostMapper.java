91533547e1c13ee42b4677c0abab90e3c1e18bce
hyun <h.zoo3963>
2023-07-16 23:05:17 +0900

    feat : PostMapper 수정 body : content가 nullable = true 이기 때문에 mapper 시 비어있으면 빈문자열을 나오게 Mapping해줌

--- src/main/java/firstserendipity/server/util/mapper/PostMapper.java
+++ src/main/java/firstserendipity/server/util/mapper/PostMapper.java
@@ -4,13 +4,17 @@ import firstserendipity.server.domain.dto.request.RequestPostDto;
 import firstserendipity.server.domain.dto.response.ResponsePostDto;
 import firstserendipity.server.domain.entity.Post;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface PostMapper {
     PostMapper INSTANCE = Mappers.getMapper(PostMapper.class);
+    @Mapping(target = "content", expression = "java(post.getContent() != null ? post.getContent() : \"\")")
     ResponsePostDto PostEntitytoResponseDto(Post post);
+    @Mapping(target = "content", expression = "java(post.getContent() != null ? post.getContent() : \"\")")
     Post RequestPostDtotoEntity(RequestPostDto requestPostDto);
+    @Mapping(target = "content", expression = "java(post.getContent() != null ? post.getContent() : \"\")")
     Post updateRequestPostDtotoEntity(RequestPostDto requestPostDto, @MappingTarget Post post);
 }

021a90c0a249ab73bd511da1feaae98cb7631e83
hyun <h.zoo3963>
2023-07-16 22:17:33 +0900

    feat : PostController body : PostService에 맞는 controller 추가

--- src/main/java/firstserendipity/server/controller/PostController.java
+++ src/main/java/firstserendipity/server/controller/PostController.java
@@ -23,12 +23,36 @@ public class PostController {
     public ResponsePostDto createPost(@RequestBody RequestPostDto requestPostDto, HttpServletRequest req) {
         return postService.createPost(requestPostDto,req);
     }
+    // 나영님의 게시글 수정
+    @PutMapping("/posts")
+    public ResponsePostDto updatePost(@PathVariable Long id, @RequestBody RequestPostDto requestPostDto, HttpServletRequest req) {
+        return postService.updatePost(id,requestPostDto,req);
+    }
+    // 나영님의 게시글 삭제
+    @DeleteMapping("/posts")
+    public ResponseEntity<String> deletePost(@PathVariable Long id, HttpServletRequest req) {
+        postService.deletePost(id,req);
+        return ResponseEntity.ok("삭제가 완료되었습니다.");
+    }
+
+
     // 전체 게시글 조회
     @GetMapping("/posts")
     public List<ResponsePostDto> getPosts(){
         return postService.getPosts();
     }
+
     // 인기글 게시글 조회
+    @GetMapping("/posts")
+    public List<ResponsePostDto> getRandomPosts(){
+        return postService.getPosts();
+    }
+
     // 추천 게시글 조회
+    @GetMapping("/posts")
+    public List<ResponsePostDto> getLikePosts(){
+        return postService.getPosts();
+    }
+
 
 }

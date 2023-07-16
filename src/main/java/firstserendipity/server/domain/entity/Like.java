f6e302beddd83536e0033dc4801b1b4d9d381e56
hyun <h.zoo3963>
2023-07-16 18:08:39 +0900

    feat : Like entity생성

---
+++ src/main/java/firstserendipity/server/domain/entity/Like.java
@@ -0,0 +1,24 @@
+package firstserendipity.server.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Like{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long postId;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+}

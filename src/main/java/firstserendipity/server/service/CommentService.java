7055c8e54900d5cb58c1b8d4ab7f442671781413
princeton <ssk0080@gmail.com>
2023-07-18 03:52:50 +0900

    Refactor: 권한 검증, 메서드 수정

--- src/main/java/firstserendipity/server/service/CommentService.java
+++ src/main/java/firstserendipity/server/service/CommentService.java
@@ -5,17 +5,18 @@ import firstserendipity.server.domain.dto.request.RequestCommentDto;
 import firstserendipity.server.domain.dto.response.ResponseCommentStatusMessageDto;
 import firstserendipity.server.domain.entity.Comment;
 import firstserendipity.server.domain.entity.Member;
-import firstserendipity.server.domain.role.Role;
+import firstserendipity.server.domain.entity.Post;
 import firstserendipity.server.repository.CommentRepository;
 import firstserendipity.server.repository.MemberRepository;
+import firstserendipity.server.repository.PostRepository;
 import firstserendipity.server.util.JwtUtil;
-import firstserendipity.server.util.mapper.CommentMapper;
-import firstserendipity.server.util.resource.ResponseResource;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,22 +24,25 @@ import java.util.Optional;
 
 import static firstserendipity.server.util.mapper.CommentMapper.*;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class CommentService {
 
     private final CommentRepository commentRepository;
     private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
     private final JwtUtil jwtUtil;
 
-    public ResponseCommentStatusMessageDto writeComment(Long postId, RequestCommentDto requestCommentDto, HttpServletRequest req) {
+    public ResponseCommentStatusMessageDto writeComment(Long id, RequestCommentDto requestCommentDto, HttpServletRequest req) {
         String successMsg = "댓글 작성이 완료되었습니다!";
         //토큰 검증 후 잘려진 토큰 반환
         String verifiedToken = validateToken(req);
         //토큰으로 멤버 식별자 찾기
         Long memberId = findMemberIdFromToken(verifiedToken);
+        Post post = postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("wrong post id!!!"));
         //작성된 내용을 entity로 변환
-        Comment comment = COMMENT_INSTANCE.commentDtoToCommentEntity(postId, memberId, requestCommentDto);
+        Comment comment = COMMENT_INSTANCE.commentDtoToCommentEntity(post, memberId, requestCommentDto);
         //저장
         Comment saveComment = commentRepository.save(comment);
         //성공 msg Dto에 담아서 build
@@ -66,6 +70,7 @@ public class CommentService {
 
     }
 
+    @Transactional
     public ResponseCommentStatusMessageDto deleteComment(Long id, HttpServletRequest req) {
         String successMsg = "댓글 삭제가 완료되었습니다!";
         //토큰 검증 후 잘려진 토큰 반환
@@ -75,25 +80,36 @@ public class CommentService {
         //요청한 유저의 MemberId 조회
         Long requestedMemberId = findMemberIdFromToken(verifiedToken);
         // 댓글을 작성한 MemberId 조회
-        Long writeMemberId = commentRepository.findMemberIdByCommentId(id);
+        log.info("before validation");
+        Comment comment = commentRepository.findByCommentId(id).orElseThrow(() -> new IllegalArgumentException("wrong comment id"));
+        Long writeMemberId = comment.getMemberId();
         //삭제 요청자가 NAYOUNG 혹은 게시글을 작성한 유저가 아니라면 exception
-        if (isNotWriteMemberOrNayoung(requestedMemberId, writeMemberId, userInfo)){
-            throw new AccessDeniedException("삭제 권한이 없습니다.");
+        if (!isWriteMemberOrNayoung(requestedMemberId, writeMemberId, userInfo)) {
+            throw new IllegalArgumentException("삭제 권한이 없습니다.");
         }
+        log.info("after validation");
+        log.info("requestedMemberId={}",requestedMemberId);
+        log.info("writeMemberId={}",writeMemberId);
+        commentRepository.deleteById(id);
         return ResponseCommentStatusMessageDto.builder()
                 .msg(successMsg).build();
     }
 
-    private boolean isNotWriteMemberOrNayoung(Long requestedMemberId, Long commentAuthorId, Claims userInfo) {
-        if (requestedMemberId.equals(commentAuthorId)) return true;
-        return userInfo.get("auth").equals(Role.NAYOUNG);
+    private boolean isWriteMemberOrNayoung(Long requestedMemberId, Long writeMemberId, Claims userInfo) {
+        if (requestedMemberId.equals(writeMemberId)) {
+            log.info("멤버id가 같음");
+            return true;
+        }
+
+        String userRole = userInfo.get("auth", String.class);
+        return userRole.equals("NAYOUNG");
     }
 
     private String validateToken(HttpServletRequest req) {
         String tokenValue = jwtUtil.getTokenFromRequest(req);
         //  jwt 토큰 substring
         String substringToken = jwtUtil.substringToken(tokenValue);
-        // jwt 토큰 검증(필요한가..?)
+        // jwt 토큰 검증
         if (!jwtUtil.validateToken(substringToken)) {
             throw new IllegalArgumentException("토큰이 유효하지 않습니다.");
         }

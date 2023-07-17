627d581e771adc8d34a1aaafc7b1696029dd010e
princeton <ssk0080@gmail.com>
2023-07-18 03:42:11 +0900

    Feat: create timestamped
    생성, 수정일시 추가할 수 있는 객체 생성

---
+++ src/main/java/firstserendipity/server/domain/entity/Timestamped.java
@@ -0,0 +1,27 @@
+package firstserendipity.server.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static jakarta.persistence.TemporalType.*;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    @Temporal(TIMESTAMP)
+    private LocalDateTime modifiedAt;
+}

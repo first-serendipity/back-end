edffc3a352dad7c14aec09da668439e9b7fe2918
princeton <ssk0080@gmail.com>
2023-07-18 03:35:15 +0900

    Refactor: comment controller
    1. 잘못된 url 주소값

--- src/main/java/firstserendipity/server/controller/CommentController.java
+++ src/main/java/firstserendipity/server/controller/CommentController.java
@@ -3,15 +3,12 @@ package firstserendipity.server.controller;
 import firstserendipity.server.domain.dto.request.RequestCommentDto;
 import firstserendipity.server.domain.dto.response.ResponseCommentStatusMessageDto;
 import firstserendipity.server.domain.dto.response.ResponseGetCommentDto;
-import firstserendipity.server.domain.dto.response.ResponseMemberStatusMessageDto;
 import firstserendipity.server.service.CommentService;
 import firstserendipity.server.util.resource.ResponseResource;
 import jakarta.servlet.http.HttpServletRequest;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,7 +26,7 @@ public class CommentController {
 
     private final CommentService commentService;
 
-    @PostMapping("/post/{id}")
+    @PostMapping("/posts/{id}")
     public ResponseEntity<ResponseResource> writeComment(
             @PathVariable Long id,
             @RequestBody RequestCommentDto requestCommentDto,
@@ -44,35 +41,16 @@ public class CommentController {
                 .build();
 
         WebMvcLinkBuilder linkBuilder = linkTo(MemberController.class);
+        WebMvcLinkBuilder postControllerLinkBuilder = linkTo(PostController.class);
 
-        responseResource.add(linkBuilder.slash("post").withSelfRel());
-        responseResource.add(Link.of("http://localhost:8080/api/posts", "getPosts"));
-//        // _links 정보 추가
-//        List<Link> links = new ArrayList<>();
-//
-//        // self 링크 생성
-//        Link selfLink = WebMvcLinkBuilder.linkTo(CommentController.class).slash("post").slash(id).withSelfRel();
-//        links.add(selfLink);
-//
-//        // getPosts 링크 생성. 근데 알아서 redirect로 가지는 url인데 여기 넣는게 맞는 지 확인 필요.
-//        Link getPostsLink = Link.of("http://localhost:8080/api/posts", "getPosts");
-//        links.add(getPostsLink);
-//
-//        // 전체 응답 생성
-//        Map<String, Object> response = new HashMap<>();
-//        response.put("_links", links);
-//
-//        // data 정보 추가
-//        List<Map<String, String>> data = new ArrayList<>();
-//        Map<String, String> dataItem = new HashMap<>();
-//        dataItem.put("msg", responseCommentStatusMessageDto.getMsg());
-//        data.add(dataItem);
-//        response.put("data", data);
+        responseResource.add(linkBuilder.slash("posts").withSelfRel());
+        responseResource.add(postControllerLinkBuilder.slash(id).withRel("getPosts"));
 
         return ResponseEntity.created(linkBuilder.slash("post").toUri()).body(responseResource);
     }
-    @GetMapping("")
-    public ResponseEntity<Map<String, Object>> getAllMyComments(HttpServletRequest req){
+
+    @GetMapping
+    public ResponseEntity<Map<String, Object>> getAllMyComments(HttpServletRequest req) {
         List<ResponseGetCommentDto> commentDtoList = commentService.getAllMyComments(req);
 
         // _links 정보 추가
@@ -92,7 +70,7 @@ public class CommentController {
 
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Map<String, Object>> deleteComment(@PathVariable Long id, HttpServletRequest req){
+    public ResponseEntity<Map<String, Object>> deleteComment(@PathVariable Long id, HttpServletRequest req) {
         ResponseCommentStatusMessageDto responseCommentStatusMessageDto = commentService.deleteComment(id, req);
 
         List<Link> links = new ArrayList<>();

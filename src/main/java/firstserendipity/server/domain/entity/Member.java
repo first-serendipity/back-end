1e65fca56f4775fc574579a9cd273aa6ed5c4e3a
hyun <h.zoo3963>
2023-07-17 13:08:10 +0900

    feat : Member Builder 추가

--- src/main/java/firstserendipity/server/domain/entity/Member.java
+++ src/main/java/firstserendipity/server/domain/entity/Member.java
@@ -3,6 +3,8 @@ package firstserendipity.server.domain.entity;
 import firstserendipity.server.domain.role.Role;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -12,6 +14,8 @@ import static lombok.AccessLevel.*;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor(access = PROTECTED)
 @NoArgsConstructor(access = PROTECTED)
 public class Member {
 

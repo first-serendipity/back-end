39d5604e025bdbd33f120195aac1bbd172841ce1
hyun <h.zoo3963>
2023-07-17 13:05:47 +0900

    feat : AfterMapping 예시 주석처리

--- src/main/java/firstserendipity/server/util/mapper/PostMapper.java
+++ src/main/java/firstserendipity/server/util/mapper/PostMapper.java
@@ -8,13 +8,29 @@ import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Mapper
 public interface PostMapper {
     PostMapper INSTANCE = Mappers.getMapper(PostMapper.class);
-    @Mapping(target = "content", expression = "java(post.getContent() != null ? post.getContent() : \"\")")
+
     ResponsePostDto PostEntitytoResponseDto(Post post);
-    @Mapping(target = "content", expression = "java(post.getContent() != null ? post.getContent() : \"\")")
+
+//
+//    default List<String> mapComments(List<Comment> comments) {
+//        return comments.stream()
+//                .map(Comment::getContent)
+//                .collect(Collectors.toList());
+//    }
+//
+//    @AfterMapping
+//    default void addCommentsToDto(Post post, @MappingTarget ResponsePostDto.Builder responsePostDtoBuilder) {
+//        responsePostDtoBuilder.comments(mapComments(post.getComments()));
+//    }
+
+
     Post RequestPostDtotoEntity(RequestPostDto requestPostDto);
-    @Mapping(target = "content", expression = "java(post.getContent() != null ? post.getContent() : \"\")")
+
     Post updateRequestPostDtotoEntity(RequestPostDto requestPostDto, @MappingTarget Post post);
 }

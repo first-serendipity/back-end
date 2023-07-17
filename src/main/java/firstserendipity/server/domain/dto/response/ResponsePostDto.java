6f25f0841d7bb64fec0d1b9da80e21fb93a33385
princeton <ssk0080@gmail.com>
2023-07-18 03:38:18 +0900

    Refactor: response post dto
    1. 수정일자 추가

--- src/main/java/firstserendipity/server/domain/dto/response/ResponsePostDto.java
+++ src/main/java/firstserendipity/server/domain/dto/response/ResponsePostDto.java
@@ -2,6 +2,7 @@ package firstserendipity.server.domain.dto.response;
 
 import firstserendipity.server.domain.entity.Comment;
 import lombok.*;
+import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -18,5 +19,6 @@ public class ResponsePostDto {
     private String content;
     private String image;
     private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
     private List<ResponseGetCommentDto> comments;
 }

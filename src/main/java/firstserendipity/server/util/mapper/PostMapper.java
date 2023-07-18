a9c8d29ea39839b00d6d8e0e5d528e33195b90cd
hyun1024 <haema3043@gmail.com>
2023-07-18 16:45:44 +0900

    Refactor: 좋아요 갯수 List에 추가

--- src/main/java/firstserendipity/server/util/mapper/PostMapper.java
+++ src/main/java/firstserendipity/server/util/mapper/PostMapper.java
@@ -5,6 +5,7 @@ import firstserendipity.server.domain.dto.response.ResponsePostDto;
 import firstserendipity.server.domain.dto.response.ResponsePostListDto;
 import firstserendipity.server.domain.entity.Post;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
@@ -13,7 +14,9 @@ public interface PostMapper {
     PostMapper POST_INSTANCE = Mappers.getMapper(PostMapper.class);
 
     ResponsePostDto postEntityToResponseDto(Post post);
-    ResponsePostListDto postEntityToResponseDtoPostList(Post post);
+
+    @Mapping(source = "postLikeCount", target = "likeCount")
+    ResponsePostListDto postEntityToResponseDtoPostList(Post post, Integer postLikeCount);
 
 //
 //    default List<String> mapComments(List<Comment> comments) {

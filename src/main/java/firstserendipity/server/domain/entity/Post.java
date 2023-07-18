971f2d37e00fd343893b8f0e0a525c3dc6a8e850
hyun1024 <haema3043@gmail.com>
2023-07-19 03:58:17 +0900

    Refactor : Post Rollback

--- src/main/java/firstserendipity/server/domain/entity/Post.java
+++ src/main/java/firstserendipity/server/domain/entity/Post.java
@@ -32,10 +32,16 @@ public class Post extends Timestamped {
     @OneToMany(mappedBy = "post")
     private List<Comment> comments;
 
-    public void updatePost(String title, String content, String image) {
+    @OneToMany(mappedBy = "post")
+    private List<Like> likes;
+
+    public Integer getLikeCount(){
+        return likes.size();
+    }
+
+    public void updatePost(String title, String content) {
         this.title = title;
         this.content = content;
-        this.image = image;
     }
 
 }

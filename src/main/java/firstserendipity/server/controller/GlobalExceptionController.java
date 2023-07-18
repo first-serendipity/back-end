7799874555d66317489a23c161a264b4afffa4d3
hyun1024 <haema3043@gmail.com>
2023-07-19 02:16:37 +0900

    Refactor : IOException handling 추가

--- src/main/java/firstserendipity/server/controller/GlobalExceptionController.java
+++ src/main/java/firstserendipity/server/controller/GlobalExceptionController.java
@@ -1,5 +1,6 @@
 package firstserendipity.server.controller;
 
+import io.jsonwebtoken.io.IOException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -7,19 +8,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalExceptionContoller {
+public class GlobalExceptionController {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> HandleIllegalArgumentException(IllegalArgumentException e){
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
     @ExceptionHandler(NullPointerException.class)
-    public ResponseEntity<String> HandleNullPointerException(NullPointerException e){
+    public ResponseEntity<String> handleNullPointerException(NullPointerException e){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> HandleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+    @ExceptionHandler(IOException.class)
+    public ResponseEntity<String> handleIOException(IOException e){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 }

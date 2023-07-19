74fcd49bdae2278636f14838fd46eb743aea8504
hyun1024 <haema3043@gmail.com>
2023-07-19 13:45:14 +0900

    Refactor: 좋아요 기능 반환값 responseResource 리팩토링

--- src/main/java/firstserendipity/server/controller/LikeController.java
+++ src/main/java/firstserendipity/server/controller/LikeController.java
@@ -1,23 +1,35 @@
 package firstserendipity.server.controller;
 
+import firstserendipity.server.domain.dto.response.ResponseMessageDto;
 import firstserendipity.server.service.LikeService;
+import firstserendipity.server.util.resource.ResponseResource;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class LikeController {
     private final LikeService likeService;
-
-    // 리턴값 수정 필요
     @PostMapping("/likes/posts/{id}")
-    public ResponseEntity<String> toggleLike(@PathVariable Long id, HttpServletRequest req){
-     return likeService.toggleLike(id,req);
+    public ResponseEntity<ResponseResource> toggleLike(@PathVariable Long id, HttpServletRequest req){
+        ResponseMessageDto responseDto = likeService.toggleLike(id,req);
+        ResponseResource responseResource = ResponseResource.builder()
+                .responseDtos(Collections.singletonList(responseDto))
+                .build();
+
+        WebMvcLinkBuilder linkBuilder = linkTo(LikeController.class);
+        responseResource.add(linkBuilder.slash("likes").slash("posts").slash(id).withSelfRel());
+        return ResponseEntity.ok(responseResource);
     }
 }

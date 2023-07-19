404c5f432022ea22e1edf493dd6afaf81b73f4e0
hyun1024 <haema3043@gmail.com>
2023-07-19 15:59:19 +0900

    Changes by gimdonghyeon

--- src/main/java/firstserendipity/server/domain/dto/request/RequestCommentDto.java
+++ src/main/java/firstserendipity/server/domain/dto/request/RequestCommentDto.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @Getter
+@Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)//TIL
 public class RequestCommentDto {
